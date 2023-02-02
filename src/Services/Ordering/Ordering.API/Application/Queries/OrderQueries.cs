4ec1bcba357f965c93608874f86bbc1f5fe46b96
ihorkostrov <lgorkostrow@gmail.com>
2023-02-02 10:59:54 +0200

    Added discount on order page

--- src/Services/Ordering/Ordering.API/Application/Queries/OrderQueries.cs
+++ src/Services/Ordering/Ordering.API/Application/Queries/OrderQueries.cs
@@ -17,10 +17,24 @@ public class OrderQueries
         connection.Open();
 
         var result = await connection.QueryAsync<dynamic>(
-            @"select o.[Id] as ordernumber,o.OrderDate as date, o.Description as description,
-                    o.Address_City as city, o.Address_Country as country, o.Address_State as state, o.Address_Street as street, o.Address_ZipCode as zipcode,
-                    os.Name as status, 
-                    oi.ProductName as productname, oi.Units as units, oi.UnitPrice as unitprice, oi.PictureUrl as pictureurl
+            @"select 
+                        o.[Id] as ordernumber,
+                        o.OrderDate as date, 
+                        o.Description as description,
+                        o.Address_City as city, 
+                        o.Address_Country as country, 
+                        o.Address_State as state, 
+                        o.Address_Street as street,
+                        o.Address_ZipCode as zipcode,
+                        os.Name as status, 
+                        oi.ProductName as productname, 
+                        oi.Units as units, 
+                        oi.UnitPrice as unitprice, 
+                        oi.PictureUrl as pictureurl,
+                        CASE WHEN o.[Discount_Amount] IS NOT NULL AND o.[Discount_DiscountConfirmed] = 1
+                            THEN o.[Discount_Amount]
+                            ELSE 0
+                        END as discount
                     FROM ordering.Orders o
                     LEFT JOIN ordering.Orderitems oi ON o.Id = oi.orderid 
                     LEFT JOIN ordering.orderstatus os on o.OrderStatusId = os.Id
@@ -39,14 +53,30 @@ public class OrderQueries
         using var connection = new SqlConnection(_connectionString);
         connection.Open();
 
-        return await connection.QueryAsync<OrderSummary>(@"SELECT o.[Id] as ordernumber,o.[OrderDate] as [date],os.[Name] as [status], SUM(oi.units*oi.unitprice) as total
+        var result = await connection.QueryAsync<dynamic>(@"SELECT 
+                        o.[Id] as ordernumber,
+                        o.[OrderDate] as [date],
+                        os.[Name] as [status], 
+                        SUM(oi.units*oi.unitprice) as total,
+                        CASE WHEN o.[Discount_Amount] IS NOT NULL AND o.[Discount_DiscountConfirmed] = 1
+                            THEN o.[Discount_Amount]
+                            ELSE 0
+                        END as discount
                     FROM [ordering].[Orders] o
                     LEFT JOIN[ordering].[orderitems] oi ON  o.Id = oi.orderid 
                     LEFT JOIN[ordering].[orderstatus] os on o.OrderStatusId = os.Id                     
                     LEFT JOIN[ordering].[buyers] ob on o.BuyerId = ob.Id
                     WHERE ob.IdentityGuid = @userId
-                    GROUP BY o.[Id], o.[OrderDate], os.[Name] 
+                    GROUP BY o.[Id], o.[OrderDate], os.[Name], o.[Discount_Amount], o.[Discount_DiscountConfirmed]
                     ORDER BY o.[Id]", new { userId });
+
+        return result.Select(x => new OrderSummary()
+        {
+            ordernumber = x.ordernumber,
+            date = x.date,
+            status = x.status,
+            total = x.total > x.discount ? x.total - x.discount : 1,
+        });
     }
 
     public async Task<IEnumerable<CardType>> GetCardTypesAsync()
@@ -59,19 +89,9 @@ public class OrderQueries
 
     private Order MapOrderItems(dynamic result)
     {
-        var order = new Order
-        {
-            ordernumber = result[0].ordernumber,
-            date = result[0].date,
-            status = result[0].status,
-            description = result[0].description,
-            street = result[0].street,
-            city = result[0].city,
-            zipcode = result[0].zipcode,
-            country = result[0].country,
-            orderitems = new List<Orderitem>(),
-            total = 0
-        };
+        decimal discount = result[0].discount;
+        decimal total = 0;
+        var orderItems = new List<Orderitem>();
 
         foreach (dynamic item in result)
         {
@@ -83,9 +103,25 @@ public class OrderQueries
                 pictureurl = item.pictureurl
             };
 
-            order.total += item.units * item.unitprice;
-            order.orderitems.Add(orderitem);
+            total += item.units * item.unitprice;
+            orderItems.Add(orderitem);
         }
+        
+        var order = new Order
+        {
+            ordernumber = result[0].ordernumber,
+            date = result[0].date,
+            status = result[0].status,
+            description = result[0].description,
+            street = result[0].street,
+            city = result[0].city,
+            zipcode = result[0].zipcode,
+            country = result[0].country,
+            orderitems = orderItems,
+            discount = result[0].discount,
+            total = total,
+            totalWithDiscount = total > discount ? total - discount : 1,
+        };
 
         return order;
     }
